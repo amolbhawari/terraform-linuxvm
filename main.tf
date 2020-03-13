acd9463fde59c2a02ae5a45ab736bf1d9f21a6be
Amol Bhawari <amol.bhawari@gmail.com>
2020-03-13 22:45:36 +0530

    new repo

--- main.tf
+++ main.tf
@@ -1,37 +1,38 @@
-resource "azurerm_resource_group" "resource_gp"{
-    name      = "demo"
-    location  = "eastus"
-}
-
 variable "prefix" {
-  default = "vm1"
+  default = "linux1"
 }
 
-resource "azurerm_public_ip" "example" {
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-resources"
+  location = "eastus"
+}
+
+resource "azurerm_public_ip" "main" {
   name                = "acceptanceTestPublicIp1"
-  location            = "West US"
-  resource_group_name = azurerm_resource_group.example.name
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.main.name
   allocation_method   = "Static"
-}
 
+
+}
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.resource_gp.location
-  resource_group_name = azurerm_resource_group.resource_gp.name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.resource_gp.name
+  resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefix       = "10.0.2.0/24"
 }
 
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.resource_gp.location
-  resource_group_name = azurerm_resource_group.resource_gp.name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 
   ip_configuration {
     name                          = "testconfiguration1"
@@ -42,16 +43,16 @@ resource "azurerm_network_interface" "main" {
 
 resource "azurerm_virtual_machine" "main" {
   name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.resource_gp.location
-  resource_group_name   = azurerm_resource_group.resource_gp.name
+  location              = azurerm_resource_group.main.location
+  resource_group_name   = azurerm_resource_group.main.name
   network_interface_ids = [azurerm_network_interface.main.id]
   vm_size               = "Standard_DS1_v2"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
-   delete_os_disk_on_termination = true
+  delete_os_disk_on_termination = true
 
   # Uncomment this line to delete the data disks automatically when deleting the VM
-   delete_data_disks_on_termination = true
+  delete_data_disks_on_termination = true
 
   storage_image_reference {
     publisher = "Canonical"
@@ -74,6 +75,6 @@ resource "azurerm_virtual_machine" "main" {
     disable_password_authentication = false
   }
   tags = {
-    environment = "demo"
+    environment = "staging"
   }
 }
