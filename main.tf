1d00b096098304ae39f3314f8122d0fb7caf0cba
amolbhawari <54710534+amolbhawari@users.noreply.github.com>
2020-06-26 14:31:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,16 +17,16 @@ resource "azurerm_public_ip" "main" {
 }
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.2.0.0/16"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
 
 resource "azurerm_subnet" "internal" {
-  name                 = "internal"
+  name                 = "subnet1"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefix       = "10.0.2.0/24"
+  address_prefix       = "10.2.0.0/24"
 }
 
 resource "azurerm_network_interface" "main" {
